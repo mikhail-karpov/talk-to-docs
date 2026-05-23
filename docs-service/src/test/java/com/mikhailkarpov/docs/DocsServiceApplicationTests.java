1fba558e5583b766a0badbd70a34a3c7b6484d76
mikhail-karpov <mikhailkarpovdev@gmail.com>
2026-05-23 12:31:40 +0400

    init: add docs-service

---
+++ docs-service/src/test/java/com/mikhailkarpov/docs/DocsServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.mikhailkarpov.docs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocsServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
